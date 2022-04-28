c579e1204584a309e7b9209b9e457597fe8d97c9
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-04-28 09:15:21 +0200

    dodanie karuzeli

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.set('view engine', 'ejs')
 app.set('path', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req,res) => {
     res.render('index')
