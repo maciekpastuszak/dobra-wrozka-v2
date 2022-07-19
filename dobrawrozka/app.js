dfa98db1c23fe6f71c9b50698cd3bce5b6215453
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-07-19 18:47:51 +0200

    nasza-misja

--- dobrawrozka/app.js
+++ dobrawrozka/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const ejsMate = require('ejs-mate');
 const path = require('path');
 
 app.set('view engine', 'ejs')
 app.set('path', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, '/public')))
+app.engine('ejs', ejsMate)
 
 app.get('/', (req,res) => {
     res.render('index')
+})
+
+app.get('/o-przedszkolu/nasza-misja', (req,res) => {
+    res.render('o-przedszkolu/nasza-misja')
 })
 
 const port = 8080 || 3002;
